0796ed9eb355aff09b24608fdd757cb3e0b1b793
zakaria5729 <zakariahossain143@gmail.com>
2025-10-13 01:43:30 +0600

    Patch

--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -26,7 +26,8 @@ func CorsMiddleware(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
-		w.Header().Set("Access-Control-Max-Age", "86400")
+		w.Header().Set("Access-Control-Max-Age", "0")
+		// w.Header().Set("Access-Control-Max-Age", "86400")
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
