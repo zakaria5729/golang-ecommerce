0f0a8e746af2b434c9ee6fcd787e5b15e34a9924
zakaria5729 <zakariahossain143@gmail.com>
2025-11-03 19:44:11 +0600

    Point to stage

--- pkg/config/env_config.go
+++ pkg/config/env_config.go
@@ -32,7 +32,7 @@ func GetConfig() *Config {
 }
 
 func GetActiveProfile() string {
-	return getEnv(c.EnvKeyActiveProfile, c.EnvLocal)
+	return getEnv(c.EnvKeyActiveProfile, c.EnvStage)
 }
 
 func loadConfig() *Config {
