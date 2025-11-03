de609b364d5e3d7c03aacf0c82c9afd183da6ed2
zakaria5729 <zakariahossain143@gmail.com>
2025-11-03 17:37:10 +0600

    Delete unused codes

--- pkg/config/config_model.go
+++ pkg/config/config_model.go
@@ -46,34 +46,3 @@ type ObjectStoreConfig struct {
 	AccessKeySecret string
 	PublicDomain    string
 }
-
-// type Config struct {
-// 	Port                       string
-// 	DBHost                     string
-// 	DBPort                     string
-// 	DBUser                     string
-// 	DBPassword                 string
-// 	DBName                     string
-// 	DBSSLMode                  string
-// 	DBShowLog                  string
-// 	JWTSecret                  string
-// 	SuperAdminEmail            string
-// 	SuperAdminPassword         string
-// 	DomainURL                  string
-// 	FcmServerKey               string
-// 	FcmUrl                     string
-// 	GoogleClientID             string
-// 	FacebookAppID              string
-// 	AppHealthCheckToken        string
-// 	SocialLoginDefaultPassword string
-// 	ObjStore                   ObjectStoreConfig
-// }
-
-// type ObjectStoreConfig struct {
-// 	Region          string
-// 	BucketName      string
-// 	AccountID       string
-// 	AccessKeyID     string
-// 	AccessKeySecret string
-// 	PublicDomain    string
-// }
